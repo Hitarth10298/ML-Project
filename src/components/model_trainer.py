c28c49c9a8a7505ed9a9c4f05cb542d1ac2b6e4d
Hitarth Shah <hitarthshah10298@gmail.com>
2023-12-04 13:04:47 -0800

    Hyperparameter Tuning Done

--- src/components/model_trainer.py
+++ src/components/model_trainer.py
@@ -33,12 +33,35 @@ class ModelTrainer:
                 "Random Forest": RandomForestRegressor(),
                 "Decision Tree": DecisionTreeRegressor(),
                 "Gradient Boosting": GradientBoostingRegressor(),
-                "Linear Regrression": LinearRegression(),
+                "Linear Regression": LinearRegression(),
                 "AdaBoost Regressor": AdaBoostRegressor(),
                 "KNeighbors Regressor": KNeighborsRegressor()
             }
 
-            model_report:dict = evaluate_models(X_train=X_train, X_test=X_test, y_train=y_train, y_test=y_test, models=models)
+            params = {
+                "Decision Tree": {
+                    'criterion': ['squared_error', 'friedman_mse', 'absolute_error', 'poisson']
+                },
+                "Random Forest": {
+                    'n_estimators': [8, 16, 32, 64, 128, 256]
+                },
+                "Gradient Boosting": {
+                    'learning_rate': [0.1, 0.01, 0.05, 0.001],
+                    'subsample': [0.6, 0.7, 0.75, 0.8, 0.85, 0.9],
+                    'n_estimators': [8, 16, 32, 64, 128, 256]
+                },
+                "Linear Regression": {},
+                "AdaBoost Regressor": {
+                    'n_estimators': [8, 16, 32, 64, 128, 256],
+                    'learning_rate': [0.1, 0.01, 0.5, 0.001]
+                }, 
+                "KNeighbors Regressor": {
+                    'weights': ['uniform', 'distance'],
+                    'algorithm': ['auto', 'ball_tree', 'kd_tree', 'brute']
+                }
+            }
+
+            model_report:dict = evaluate_models(X_train=X_train, X_test=X_test, y_train=y_train, y_test=y_test, models=models, param=params)
             best_model_score = max(sorted(model_report.values()))
 
             best_model_name = list(model_report.keys()) [
